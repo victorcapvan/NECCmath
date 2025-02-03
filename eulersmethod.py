ccaaa2187970d7541c3a27c40ca3d92fc7970ed2
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2025-02-03 14:50:43 -0500

    Add files via upload

--- eulersmethod.py
+++ eulersmethod.py
@@ -1,20 +1,17 @@
 import typing
+import numpy as np
+import pprint
 
 def f(f: typing.Callable[[float, float], float], initial: tuple[float, float], h: float, n: int) -> None:
-    (x, y) = initial
-    x_list = []
-    y_list = []
-    f_list = []
+    x, y = initial
+    result_list = []
     for i in range(n):
         f_n = f(x, y)
-        f_list.append(f_n)
-        y += h*f_n
+        y += h * f_n
         x += h
-        x_list.append(x)
-        y_list.append(y)   
-    print(f"x_list {x_list}")
-    print(f"y_list {y_list}")
-    print(f"f_list {f_list}")
+        result_list.append((i + 1, x, y, f_n)) # (n, x_n, y_n, f_n)
+    result_matrix = np.array(result_list)
+    pprint.pprint(result_matrix)
     return
 
-f(lambda x, y: ((1/x)*(y**2 + y)), (6, 3), 0.2, 4)
+f(lambda x, y: ((9/x**2) - (y/x) - y**2), (2, -(3/2)), 0.1, 10)
