8d105918a9e5a9956ecc94d854b3d972413cd3bc
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2025-02-03 23:00:33 -0500

    Add files via upload

--- eulersmethod.py
+++ eulersmethod.py
@@ -1,18 +1,14 @@
-import typing
 import numpy as np
-import pprint
+import pprint as pp
+import typing as t
 
-def f(f: typing.Callable[[float, float], float], initial: tuple[float, float], h: float, n: int) -> None:
+def f(f: t.Callable[[float, float], float], initial: tuple[float, float], h: float, n: int) -> None:
     (x, y) = initial
-    result_list = []
-    result_list.append((0, x, y, f(x,y)))
+    result_list = [(0, x, y, f(x, y))]
     for i in range(n):
-        f_n = f(x, y)
-        y += h * f_n
-        x += h
-        result_list.append((i + 1, x, y, f_n)) # (n, x_n, y_n, f_n)
-    result_matrix = np.array(result_list)
-    pprint.pprint(result_matrix)
+        (x, y) = (x + h, y + h * (f_n := f(x, y)))
+        result_list.append((i + 1, x, y, f_n))
+    pp.pprint(np.array(result_list))
     return
 
-f(lambda x, y: ((9/x**2) - (y/x) - y**2), (2, -(3/2)), 0.1, 10)
+f(lambda x, y: 9/x**2 - y/x - y**2, (2, -3/2), 0.1, 10)
