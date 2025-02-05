5a8031ab8ff0f062559131261a8372185dcd85c8
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2025-02-05 09:23:43 -0500

    Add files via upload

--- ODEsapproxmethods.py
+++ ODEsapproxmethods.py
@@ -1,11 +1,12 @@
 import numpy as np, pprint as pp, typing as t
 
 def eulersMethod(f: t.Callable[[float, float], float], initial_conditions: tuple[float, float], h: float, n: int, show_all: bool = True) -> None:
-    (x_n, y_n), result_list = initial_conditions, [(0, *initial_conditions, f(*initial_conditions))]
+    x_n, y_n = initial_conditions
+    solution_set = [(0, *initial_conditions, f(*initial_conditions))]
     for i in range(n):
         (x_n, y_n) = (x_n + h, y_n + h * (f_n := f(x_n, y_n)))
-        result_list.append((i + 1, x_n, y_n, f_n))     
-    pp.pprint(np.array(result_list)) if show_all else print(result_list[-1]); print("\n")
+        solution_set.append((i + 1, x_n, y_n, f_n))     
+    pp.pprint(np.array(solution_set)) if show_all else print(solution_set[-1]); print("\n")
     return
 
 # number 8 from 1.4
